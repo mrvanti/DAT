6315fa4e36b5ecf5ae64ca4ef2a5b66facda4b7a
MartinJansson <mrvanti@gmail.com>
2024-03-16 19:56:06 +0100

    Start on holding timers

--- DAT/ExternalWindow.xaml.cs
+++ DAT/ExternalWindow.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace DAT
 {
     /// <summary>
@@ -20,28 +10,20 @@ namespace DAT
         public ExternalWindow()
         {
             InitializeComponent();
-            this.Closing += OnClose;
+            Closing += OnClose;
         }
 
-        private void Clock_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
         private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true;
-            this.Visibility = Visibility.Hidden;
+            Visibility = Visibility.Hidden;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            
-        }
         private void ExternalWindow_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             double controlsize = ((e.NewSize.Width / 12) / 3 * 4);
             Application.Current.Resources.Remove("ControlFontSize");
-            Application.Current.Resources.Add("ControlFontSize", controlsize);            
+            Application.Current.Resources.Add("ControlFontSize", controlsize);
         }
     }
 }
