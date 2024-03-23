81ba930577c916e05a5b0a4369f758f9bd6e00f3
MartinJansson <mrvanti@gmail.com>
2024-03-23 18:58:40 +0100

    All them changes

--- DAT/MainWindow.xaml.cs
+++ DAT/MainWindow.xaml.cs
@@ -1,14 +1,9 @@
-﻿
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.Windows;
 using System.Windows.Media;
 
 namespace DAT
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private const string _blueWin = "Blå vinner";
@@ -17,6 +12,8 @@ namespace DAT
         private int _blueScore = 7;
         private int _whiteScore = 7;
         private ExternalWindow _externalWindow;
+        private readonly Thickness fatBorder = new Thickness(4.0);
+        private readonly Thickness lightBorder = new Thickness(2.0);
 
 
         public MainWindow()
@@ -30,8 +27,7 @@ namespace DAT
 
             //update label text            
             _clockTimer.Elapsed += OnTimeChanged;
-
-            ClockTicks = 0;
+            Closing += OnClose;
         }
 
         private void CheckWinner()
@@ -41,52 +37,47 @@ namespace DAT
                 if (_blueScore - _whiteScore >= 30 || _whiteScore - _blueScore >= 30 || MustStop)
                 {
                     CheckWinningScore();
-                }                
+                }
             }
         }
         #region WinnerLogic
 
         private void CheckWinningScore()
         {
+            ResetTextboxBorder();
+            var winner = "";
             if (_blueScore > _whiteScore)
             {
-                ResetTextboxBorder();
-                _externalWindow.Result_external.Text = _blueWin;
+                winner = _blueWin;
                 _externalWindow.Result_external.Background = Brushes.Red;
-                _externalWindow.Result_external.Foreground = Brushes.Blue;
-                _externalWindow.BlueScore_external.BorderThickness = new Thickness(4.0);
+                _externalWindow.BlueScore_external.BorderThickness = fatBorder;
                 SetWinnerTextboxBorder();
-                return;
             }
 
             if (_whiteScore > _blueScore)
             {
-                ResetTextboxBorder();
-                _externalWindow.Result_external.Text = _whiteWin;
+                winner = _whiteWin;
                 _externalWindow.Result_external.Background = Brushes.Red;
-                _externalWindow.Result_external.Foreground = Brushes.White;
-                _externalWindow.WhiteScore_external.BorderThickness = new Thickness(4.0);
+                _externalWindow.WhiteScore_external.BorderThickness = fatBorder;
                 SetWinnerTextboxBorder();
-                return;
             }
+
             if (_whiteScore == _blueScore)
             {
-                _externalWindow.Result_external.Text = _draw;
+                winner = _draw;
                 _externalWindow.Result_external.BorderBrush = Brushes.Red;
-                _externalWindow.Result_external.Background = Brushes.White;
                 _externalWindow.WhiteScore_external.BorderBrush = Brushes.Red;
                 _externalWindow.BlueScore_external.BorderBrush = Brushes.Red;
-                _externalWindow.BlueScore_external.BorderThickness = new Thickness(4.0);
-                _externalWindow.WhiteScore_external.BorderThickness = new Thickness(4.0);
-                return;
+                _externalWindow.BlueScore_external.BorderThickness = fatBorder;
+                _externalWindow.WhiteScore_external.BorderThickness = fatBorder;
             }
-            ResetTextboxBorder();
+            _externalWindow.Result_external.Text = winner;
         }
 
         private void SetWinnerTextboxBorder()
         {
             _externalWindow.Result_external.BorderBrush = Brushes.Red;
-            _externalWindow.Result_external.BorderThickness = new Thickness(4.0);
+            _externalWindow.Result_external.BorderThickness = fatBorder;
         }
 
         #endregion
@@ -94,13 +85,13 @@ namespace DAT
         private void ResetTextboxBorder()
         {
             _externalWindow.Result_external.BorderBrush = Brushes.Black;
-            _externalWindow.Result_external.BorderThickness = new Thickness(2.0);
+            _externalWindow.Result_external.BorderThickness = fatBorder;
 
             _externalWindow.WhiteScore_external.BorderBrush = Brushes.Black;
-            _externalWindow.WhiteScore_external.BorderThickness = new Thickness(2.0);
+            _externalWindow.WhiteScore_external.BorderThickness = fatBorder;
 
             _externalWindow.BlueScore_external.BorderBrush = Brushes.Black;
-            _externalWindow.BlueScore_external.BorderThickness = new Thickness(2.0);
+            _externalWindow.BlueScore_external.BorderThickness = fatBorder;
         }
 
 
@@ -108,7 +99,7 @@ namespace DAT
 
         private const string _startTimeDisplay = "2:00";
         private readonly System.Timers.Timer _clockTimer = new System.Timers.Timer(1000.0);
-        private int MaxTime => 2*60;
+        private static int MaxTime => 2 * 60;
         private bool MustStop => (MaxTime - ClockTicks) < 0;
         private int ClockTicks { get; set; }
         public TimeSpan TimeLeft =>
@@ -125,7 +116,9 @@ namespace DAT
                 {
                     CheckWinner();
                 }
-                Clock_label.Text = TimeLeft.ToString(@"m\:ss"); ;
+
+                Clock_label.Text = TimeLeft.ToString(@"m\:ss");
+
                 if (_externalWindow != null)
                 {
                     _externalWindow.Clock_external.Text = TimeLeft.ToString(@"m\:ss");
@@ -179,7 +172,8 @@ namespace DAT
         {
             _holdTimer.Stop();
             _holdTimer.Elapsed -= OnBlueHoldTimerChanged;
-            _externalWindow.HoldTimerBlueExt.Text = _holderReset;
+            _externalWindow.HoldTimerBlueExt.Text = "";
+            _externalWindow.HoldScoreTypeBlueExt.Text = "";
             BlueHoldTimer.Text = _holderReset;
             HolderTimerTicks = 0;
         }
@@ -230,8 +224,8 @@ namespace DAT
         {
             _holdTimer.Stop();
             _holdTimer.Elapsed -= OnWhiteHoldTimerChanged;
-            _externalWindow.HoldTimerWhiteExt.Text = _holderReset;
-            _externalWindow.HoldTimerWhiteExt.Visibility = Visibility.Hidden;
+            _externalWindow.HoldTimerWhiteExt.Text = "";
+            _externalWindow.HoldScoreTypeWhiteExt.Text = "";
             WhiteHoldTimer.Text = _holderReset;
             HolderTimerTicks = 0;
         }
@@ -283,10 +277,8 @@ namespace DAT
 
         private void VisaExtern_Click(object sender, RoutedEventArgs e)
         {
-            if (_externalWindow == null)
-            {
-                _externalWindow = new ExternalWindow();
-            }
+            _externalWindow ??= new ExternalWindow();
+
             if (_externalWindow.Visibility == Visibility.Hidden)
             {
                 _externalWindow.Visibility = Visibility.Visible;
@@ -295,7 +287,6 @@ namespace DAT
             {
                 _externalWindow.Show();
                 _externalWindow.Owner = this;
-
             }
         }
 
@@ -380,14 +371,21 @@ namespace DAT
             _externalWindow.Result_external.Background = Brushes.White;
 
             //White
+            WhiteHoldReset_Click(sender, e);
             _externalWindow.WhiteScore_external.Text = resetScore;
             WhiteScore.Text = resetScore;
             _whiteScore = 7;
 
             //Blue
+            BlueHoldReset_Click(sender, e);
             _externalWindow.BlueScore_external.Text = resetScore;
             BlueScore.Text = resetScore;
             _blueScore = 7;
+        }
+
+        private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
     }
